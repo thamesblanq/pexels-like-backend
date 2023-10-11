f696a8da26c6817aca549267a2622d177c041715
blanq31 <obiwulugodswill@gmail.com>
2023-10-11 16:22:44 +0100

    upgrading custom logger

--- util/logger.js
+++ util/logger.js
@@ -1,20 +1,23 @@
-const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
 
-// Log directory path
+// Log directory path and check if directory exists if not create a new one
 const logDirectory = path.resolve(__dirname, '..', 'log');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-// Ensure log directory exists
-if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory);
-}
-
-// Log file path
-const logFilePath = path.join(logDirectory, 'access.log');
+//custom filename with date
+const getLogFilePath = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    return path.join(logDirectory, `access_${year}-${month}-${day}.log`);
+};
 
 // Create a writable stream
-const logStream = fs.createWriteStream(logFilePath, { flags: 'a' });
+const logStream = fs.createWriteStream(getLogFilePath(), { flags: 'a' });
 
 module.exports = {
     dev: morgan('dev'),
